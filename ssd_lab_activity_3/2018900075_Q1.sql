6dbeb423f3e299a6cd38ba5fbfddd12cca7025a9
nsmadan-pgssp <madan.ns@gmail.com>
2022-08-18 15:28:44 +0530

    Create 2018900075_Q1.sql
    Q1 COMPLETED

---
+++ ssd_lab_activity_3/2018900075_Q1.sql
@@ -0,0 +1,5 @@
+select CONCAT(emp.fname, ' ', emp.minit, ' ',emp.lname)  "full Name" ,emp.ssn,dpt.dnumber,dpt.dname  from   employee emp,department dpt 
+where dpt.mgr_ssn=emp.Ssn 
+and emp.ssn in (select distinct emp.super_ssn  from
+employee emp,(select essn,sum(hours) hsum from works_on where hours is not null group by essn having hsum < 40) emphr
+where emphr.essn=emp.ssn);
